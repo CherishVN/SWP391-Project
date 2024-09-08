9fb7d087cfa779e29edff1dfc0dea8f0dc41a483
NguyenHoQuocThang <thangnhqse183534@fpt.edu.vn>
2024-09-08 19:05:57 +0700

    delete file cs

--- ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EventFlowerExchange.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
